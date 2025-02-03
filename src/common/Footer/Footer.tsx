89b51ea829a974078b14b424d5361f2655b08172
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-03 22:58:04 +0530

    create navigations

---
+++ src/common/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Footer: Component = () => {
+  return (
+    <div class="flex flex-col w-full min-h-screen items-center justify-center">
+        Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/common/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Navbar: Component = () => {
+  return (
+    <div class="flex flex-col w-full min-h-screen items-center justify-center">
+        Navbar
+    </div>
+  );
+};
+
+export default Navbar;
