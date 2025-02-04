0d08e7c11620d3ea8af8ff0eb68352bd11d42d68
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-04 22:33:46 +0530

    update navbar

--- src/common/Navbar/Navbar.tsx
+++ src/common/Navbar/Navbar.tsx
@@ -1,9 +1,21 @@
 import type { Component } from 'solid-js';
 
+import logo from '../../assets/logo/gmp-share.svg';
+
 const Navbar: Component = () => {
   return (
     <div class="flex flex-col w-full h-auto py-4 border border-[#acacac] items-center justify-center">
-        Navbar
+      <div class='w-11/12 h-full flex flex-row items-center justify-between'>
+        <a href="/">
+          <img src={logo} alt="gmpshare logo" class='w-28 h-auto' />
+        </a>
+        <div class='flex flex-row gap-6 items-center'>
+          <div>About</div>
+          <div>IPOs</div>
+          <div>Features</div>
+          <div>Reach Us</div>
+        </div>
+      </div>
     </div>
   );
 };
