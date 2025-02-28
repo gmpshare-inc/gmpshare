0fa1d9e8089c1a3bbe4465f83af70c82df7ce81f
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-02-28 23:18:20 +0530

    update homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,34 @@ import type { Component } from 'solid-js';
 import Navbar from '../common/Navbar/Navbar';
 import Footer from '../common/Footer/Footer';
 
+import boi from '../assets/boi.png';
+
 export const Home: Component = () => {
     return (
         <div>
             <Navbar />
-            <div class="flex flex-col w-full min-h-screen items-center justify-center">
+            <div class="flex flex-col w-full min-h-screen items-center justify-center pt-4">
+
+                <div class='w-11/12 h-full flex flex-col items-center justify-center overflow-hidden'>
+
+                    <div class='w-full h-auto flex flex-col items-center justify-center gap-3 mb-6'>
+                        <h1 class='text-3xl font-bold'>Gmpshare: For latest GMP data, IPO details and insights.</h1>
+                        <hr class='h-[0.5px]' />
+                    </div>
+
+                    <div class='w-full h-auto flex flex-col items-center justify-center gap-2 mb-4'>
+                        <img src={boi} alt='boy' class='w-[42rem]' />
+                        <p>Do what you do the best!</p>
+                    </div>
+
+                    <div class='w-full px-8 border-l-4 border-l-[#232472]'>
+                        <div class='flex flex-row gap-2 items-center text-[#232472]'>
+                            <span>Note</span>
+                        </div>
+                        <div>&#x2022; Here you can access all the features of gmpshare admin portal, after login.</div>
+                    </div>
+
+                </div>
 
                 <main class="flex md:flex-row flex-col w-11/12 min-h-screen items-start justify-start gap-10 my-4">
                     <div class="flex flex-col md:w-10/12 w-full h-full">
